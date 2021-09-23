12b00d0948c846e2a1b174ccc17df3169041a4d1
manzajp <manzajp.23@outlook.com>
2021-09-23 22:07:41 +0800

    update to readme and created separate pages in separate dart files

--- README.md
+++ README.md
@@ -1,16 +1,4 @@
-# sagile_mobile_main
+# SAgile Mobile
 
-Mobile front-end for SAgile made using Flutter framework.
+Mobile front-end for SAgile made using the Flutter framework.
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,29 @@
 import 'package:flutter/material.dart';
 
+import 'pages/homepage.dart';
+import 'pages/login.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(SAgile());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class SAgile extends StatelessWidget {
+  var _login = false;
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      title: 'SAgile Mobile',
+      theme: ThemeData.from(
+        colorScheme: ColorScheme.fromSwatch(
+            primarySwatch: Colors.indigo, accentColor: Colors.indigo[100]),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: _FrontPage(),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-          ],
-        ),
-      ),
-    );
+  Widget _FrontPage() {
+    if (_login) return Homepage();
+    return Login();
   }
 }
