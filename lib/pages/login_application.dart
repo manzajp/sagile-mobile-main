13ecf9a1feebd84e477dca4fc5e49ec6c00a18e3
farisfaisal01 <farisfaisal100@gmail.com>
2022-01-19 02:28:12 +0800

    login update

--- lib/pages/login_application.dart
+++ lib/pages/login_application.dart
@@ -1,206 +1,152 @@
-import '../flutter_flow/flutter_flow_theme.dart';
-import '../flutter_flow/flutter_flow_util.dart';
-import '../flutter_flow/flutter_flow_widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
-class HomePageWidget extends StatefulWidget {
-  const HomePageWidget({Key key}) : super(key: key);
-
-  @override
-  _HomePageWidgetState createState() => _HomePageWidgetState();
+void main() {
+  runApp(new MyApp());
 }
-
-class _HomePageWidgetState extends State<HomePageWidget> {
-  TextEditingController textController1;
-  TextEditingController textController2;
-  final scaffoldKey = GlobalKey<ScaffoldState>();
-
-  @override
-  void initState() {
-    super.initState();
-    textController1 = TextEditingController();
-    textController2 = TextEditingController();
-  }
+class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: scaffoldKey,
-      backgroundColor: Color(0xFFF5F5F5),
-      body: SafeArea(
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Row(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Container(
-                  width: 375,
-                  height: 200,
-                  decoration: BoxDecoration(
-                    color: Color(0xFFF5F5F5),
-                  ),
-                  child: Padding(
-                    padding: EdgeInsetsDirectional.fromSTEB(0, 60, 0, 0),
-                    child: Text(
-                      'SAgile',
-                      textAlign: TextAlign.center,
-                      style: FlutterFlowTheme.title1.override(
-                        fontFamily: 'Lexend Deca',
-                        fontSize: 50,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Column(
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    Padding(
-                      padding: EdgeInsetsDirectional.fromSTEB(35, 0, 0, 0),
-                      child: Container(
-                        width: 300,
-                        decoration: BoxDecoration(),
-                        child: Padding(
-                          padding: EdgeInsetsDirectional.fromSTEB(0, 150, 0, 0),
-                          child: TextFormField(
-                            controller: textController1,
-                            obscureText: false,
-                            decoration: InputDecoration(
-                              labelText: 'Username',
-                              labelStyle: FlutterFlowTheme.bodyText1,
-                              hintText: 'Your username...',
-                              hintStyle: FlutterFlowTheme.bodyText1,
-                              enabledBorder: UnderlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: Color(0x00000000),
-                                  width: 1,
-                                ),
-                                borderRadius: BorderRadius.circular(8),
-                              ),
-                              focusedBorder: UnderlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: Color(0x00000000),
-                                  width: 1,
-                                ),
-                                borderRadius: BorderRadius.circular(8),
-                              ),
-                              prefixIcon: Icon(
-                                Icons.account_circle,
-                              ),
-                            ),
-                            style: FlutterFlowTheme.bodyText1,
-                          ),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsetsDirectional.fromSTEB(35, 10, 0, 0),
-                      child: Container(
-                        width: 300,
-                        decoration: BoxDecoration(),
-                        child: TextFormField(
-                          controller: textController2,
-                          obscureText: false,
-                          decoration: InputDecoration(
-                            labelText: 'Password',
-                            labelStyle: FlutterFlowTheme.bodyText1,
-                            hintText: 'Your password...',
-                            hintStyle: FlutterFlowTheme.bodyText1,
-                            enabledBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Color(0x00000000),
-                                width: 1,
-                              ),
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            focusedBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Color(0x00000000),
-                                width: 1,
-                              ),
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            prefixIcon: Icon(
-                              Icons.lock,
-                            ),
-                          ),
-                          style: FlutterFlowTheme.bodyText1,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            Padding(
-              padding: EdgeInsetsDirectional.fromSTEB(0, 15, 0, 0),
-              child: FFButtonWidget(
-                onPressed: () {
-                  print('Button pressed ...');
-                },
-                text: 'Login',
-                options: FFButtonOptions(
-                  width: 200,
-                  height: 50,
-                  color: FlutterFlowTheme.primaryColor,
-                  textStyle: FlutterFlowTheme.subtitle2.override(
-                    fontFamily: 'Lexend Deca',
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  borderSide: BorderSide(
-                    color: Colors.transparent,
-                    width: 1,
-                  ),
-                  borderRadius: 12,
-                ),
-              ),
-            ),
-            Row(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Padding(
-                  padding: EdgeInsetsDirectional.fromSTEB(30, 40, 0, 6),
-                  child: Text(
-                    'Don’t have an account yet? ',
-                    style: FlutterFlowTheme.bodyText1,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsetsDirectional.fromSTEB(0, 30, 0, 0),
-                  child: FFButtonWidget(
-                    onPressed: () {
-                      print('Button pressed ...');
-                    },
-                    text: 'Register',
-                    options: FFButtonOptions(
-                      width: 100,
-                      height: 32,
-                      color: Color(0xFFF5F5F5),
-                      textStyle: FlutterFlowTheme.subtitle2.override(
-                        fontFamily: 'Poppins',
-                        color: Colors.black,
-                      ),
-                      borderSide: BorderSide(
-                        color: Colors.transparent,
-                        width: 1,
-                      ),
-                      borderRadius: 12,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
+    return new MaterialApp(
+      title: 'Generated App',
+      theme: new ThemeData(
+        brightness:Brightness.dark,
+        primarySwatch: Colors.blue,
+        primaryColor: const Color(0xFF212121),
+        accentColor: const Color(0xFF64ffda),
+        canvasColor: const Color(0xFF303030),
       ),
+      home: new MyHomePage(),
     );
   }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+    @override
+    Widget build(BuildContext context) {
+      return new Scaffold(
+        appBar: new AppBar(
+          title: new Text('App Name'),
+          ),
+        body:
+          new Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              new Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  new Container(
+                    child:
+                      new Text(
+                      "SAgile",
+                        style: new TextStyle(fontSize:50.0,
+                        color: const Color(0xFFffffff),
+                        fontWeight: FontWeight.w700,
+                        fontFamily: "Roboto"),
+                      ),
+    
+                    padding: const EdgeInsets.fromLTRB(1.0, 30.0, 1.0, 1.0),
+                    alignment: Alignment.center,
+                  )
+                ]
+    
+              ),
+    
+              new Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  new Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.max,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      new Container(
+                        child:
+                          new TextField(
+                            style: new TextStyle(fontSize:12.0,
+                            color: const Color(0xFFffffff),
+                            fontWeight: FontWeight.w200,
+                            fontFamily: "Roboto"),
+                          ),
+    
+                        padding: const EdgeInsets.fromLTRB(1.0, 80.0, 1.0, 1.0),
+                        alignment: Alignment.center,
+                      ),
+    
+                      new Container(
+                        child:
+                          new TextField(
+                            style: new TextStyle(fontSize:12.0,
+                            color: const Color(0xFFffffff),
+                            fontWeight: FontWeight.w200,
+                            fontFamily: "Roboto"),
+                          ),
+    
+                        padding: const EdgeInsets.fromLTRB(1.0, 20.0, 1.0, 1.0),
+                        alignment: Alignment.center,
+                      ),
+    
+                      new FlatButton(key:null, onPressed:buttonPressed,
+                        child:
+                          new Text(
+                          "Login",
+                            style: new TextStyle(fontSize:14.0,
+                            color: const Color(0xFFffffff),
+                            fontWeight: FontWeight.w400,
+                            fontFamily: "Roboto"),
+                          )
+                        ),
+    
+                      new Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        mainAxisSize: MainAxisSize.max,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          new Text(
+                          "Don’t have an account yet? ",
+                            style: new TextStyle(fontSize:12.0,
+                            color: const Color(0xFFffffff),
+                            fontWeight: FontWeight.w200,
+                            fontFamily: "Roboto"),
+                          ),
+    
+                          new FlatButton(key:null, onPressed:buttonPressed,
+                            child:
+                              new Text(
+                              "Register",
+                                style: new TextStyle(fontSize:12.0,
+                                color: const Color(0xFFffffff),
+                                fontWeight: FontWeight.w400,
+                                fontFamily: "Roboto"),
+                              )
+                            )
+                        ]
+    
+                      )
+                    ]
+    
+                  )
+                ]
+    
+              )
+            ]
+    
+          ),
+    
+      );
+    }
+    void buttonPressed(){}
+    
 }
