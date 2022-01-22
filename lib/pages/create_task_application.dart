b8d1e1b6377604b781b2772ecd280e20e940ce3f
manzajp <manzajp.23@outlook.com>
2022-01-22 22:10:39 +0800

    added create task and date picker

--- lib/pages/create_task_application.dart
+++ lib/pages/create_task_application.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class TaskCreateWidget extends StatefulWidget {
@@ -8,6 +9,9 @@ class TaskCreateWidget extends StatefulWidget {
 }
 
 class _TaskCreateWidgetState extends State<TaskCreateWidget> {
+  bool notifyUpdate = false;
+  DateTime taskDate = DateUtils.dateOnly(DateTime.now());
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,99 +29,134 @@ class _TaskCreateWidgetState extends State<TaskCreateWidget> {
           children: [
             Container(
               padding: const EdgeInsets.symmetric(horizontal: 50),
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.max,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.only(bottom: 15),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Name of the task:",
-                            style: GoogleFonts.robotoCondensed(
-                                fontSize: 16.0, fontWeight: FontWeight.normal),
-                          ),
-                          TextField(
-                            style: GoogleFonts.robotoCondensed(
-                                fontSize: 16.0, fontWeight: FontWeight.normal),
-                            decoration: const InputDecoration(
-                              hintText: 'e.g. Sprint Planning Week 1',
+              child: Form(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.max,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.only(bottom: 15),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Name of the task:",
+                              style: GoogleFonts.robotoCondensed(
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.normal),
                             ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.only(bottom: 15),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Name of the task:",
-                            style: GoogleFonts.robotoCondensed(
-                                fontSize: 16.0, fontWeight: FontWeight.normal),
-                          ),
-                          TextField(
-                            style: GoogleFonts.robotoCondensed(
-                                fontSize: 16.0, fontWeight: FontWeight.normal),
-                            decoration: const InputDecoration(
-                              hintText: 'e.g. Sprint Planning Week 1',
+                            TextField(
+                              style: GoogleFonts.robotoCondensed(
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.normal),
+                              decoration: const InputDecoration(
+                                hintText: 'e.g. Sprint Planning Week 1',
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.only(bottom: 15),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Name of the task:",
-                            style: GoogleFonts.robotoCondensed(
-                                fontSize: 16.0, fontWeight: FontWeight.normal),
-                          ),
-                          TextField(
-                            style: GoogleFonts.robotoCondensed(
-                                fontSize: 16.0, fontWeight: FontWeight.normal),
-                            decoration: const InputDecoration(
-                              hintText: 'e.g. Sprint Planning Week 1',
+                      Container(
+                        padding: const EdgeInsets.only(bottom: 15),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Description of the task:",
+                              style: GoogleFonts.robotoCondensed(
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.normal),
                             ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.only(bottom: 15),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Name of the task:",
-                            style: GoogleFonts.robotoCondensed(
-                                fontSize: 16.0, fontWeight: FontWeight.normal),
-                          ),
-                          TextField(
-                            style: GoogleFonts.robotoCondensed(
-                                fontSize: 16.0, fontWeight: FontWeight.normal),
-                            decoration: const InputDecoration(
-                              hintText: 'e.g. Sprint Planning Week 1',
+                            TextField(
+                              maxLines: 3,
+                              style: GoogleFonts.robotoCondensed(
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.normal),
+                              decoration: const InputDecoration(
+                                hintText:
+                                    'e.g. Discuss on Project Background, Project Backlogs and Sprint Backlog 1',
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
-                    ),
-                    TextButton(
-                        onPressed: buttonPressed,
-                        child: Text(
-                          "Create a new task!",
-                          style: GoogleFonts.robotoCondensed(
-                              fontSize: 16, fontWeight: FontWeight.normal),
-                        ))
-                  ]),
+                      Container(
+                        padding: const EdgeInsets.only(bottom: 15),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Date:",
+                              style: GoogleFonts.robotoCondensed(
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.normal),
+                            ),
+                            TextButton(
+                                onPressed: () {
+                                  DatePicker.showDatePicker(context,
+                                      showTitleActions: true,
+                                      theme: const DatePickerTheme(
+                                          headerColor: Colors.blue,
+                                          backgroundColor: Colors.white,
+                                          itemStyle: TextStyle(
+                                              color: Colors.blue, fontSize: 16),
+                                          doneStyle: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 16),
+                                          cancelStyle: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 16)),
+                                      onConfirm: (date) {
+                                    setState(() {
+                                      taskDate = date;
+                                    });
+                                  },
+                                      currentTime: taskDate,
+                                      locale: LocaleType.en);
+                                },
+                                child: Text("${taskDate.day}/"
+                                    "${taskDate.month}"
+                                    "/${taskDate.year}")),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.only(bottom: 15),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Notify all members on changes?",
+                              style: GoogleFonts.robotoCondensed(
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.normal),
+                            ),
+                            Checkbox(
+                                value: notifyUpdate,
+                                onChanged: (value) {
+                                  setState(() {
+                                    notifyUpdate = value!;
+                                  });
+                                }),
+                          ],
+                        ),
+                      ),
+                      ElevatedButton(
+                          onPressed: buttonPressed,
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 15, horizontal: 15),
+                            child: Text(
+                              "Create a new task!",
+                              style: GoogleFonts.robotoCondensed(
+                                  fontSize: 16, fontWeight: FontWeight.normal),
+                            ),
+                          ))
+                    ]),
+              ),
             ),
           ]),
     );
